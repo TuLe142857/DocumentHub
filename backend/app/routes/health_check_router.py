ce2a8acd9ab910bfbff815aa46ce127eff16500c
tule <tuleaibk@gmail.com>
2026-04-03 17:17:08 +0700

    fix health check route endpoint

--- backend/app/routes/health_check_router.py
+++ backend/app/routes/health_check_router.py
@@ -5,6 +5,6 @@ from app.core import APIResponse, ResponseErrorSchema, ResponseSuccessSchema
 router = APIRouter(prefix="/health", tags=["Health Check"])
 
 
-@router.get("/", response_model=ResponseSuccessSchema)
+@router.get("", response_model=ResponseSuccessSchema)
 def health_check():
     return APIResponse.ok()
