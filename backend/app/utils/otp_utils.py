b272c2705f39e8f53b3e6e14af2f40b967afbf74
tule <tuleaibk@gmail.com>
2026-04-05 14:21:38 +0700

    update backend: ....

--- backend/app/utils/otp_utils.py
+++ backend/app/utils/otp_utils.py
@@ -3,6 +3,10 @@ import string
 
 
 def generate_otp(size: int = 8, digits: str = None) -> str:
+    if size <= 0:
+        raise ValueError("size must be > 0")
+    if len(digits) <= 0:
+        raise ValueError("len of digits must be > 0")
     if not digits:
         digits = string.ascii_uppercase + string.digits
     return "".join(random.choices(digits, k=size))
